ed98adabb230f9055bc609ad20a82b43d42b877c
msr3cob <80315138+msr3cob@users.noreply.github.com>
2021-03-10 10:08:10 +0100

    Create List.py

---
+++ List.py
@@ -0,0 +1,9 @@
+print("List __name__ = %s" % __name__)
+if __name__ == "__main__":
+    print("The file is run directly")
+else:
+    print("The file is being imported")
+
+list =['srihari','maddirala','python']
+cs_course={'History','Arts','Math','Design'}
+art_cousres={'History','Physics','Arts','Chemistry'}
